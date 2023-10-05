3eb52f3770af3686983f57ebff3efc814948f7ec
awabaqibb <94635789+awabaqibb@users.noreply.github.com>
2023-10-05 19:31:48 +0500

    ok0

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Define an object to map grades to their values
+const gradeValues = {
+  "A*": 94,
+  A: 95,
+  B: 75,
+  C: 65,
+  D: 55,
+  E: 45,
+};
+
+// Define an object to specify custom marks for certain subjects and grades
+const customMarks = {
+  Maths: {
+    "A*": 95,
+  },
+  Islamiat: {
+    "A*": 95,
+  },
+  Urdu: {
+    "A*": 93,
+  },
+};
+
 // Define an array of subject names
 const subjects = [
   "Maths",
@@ -24,12 +47,12 @@ function generateSubjectRadios() {
     const gradeOptions = document.createElement("div");
     gradeOptions.classList.add("grade-options");
 
-    const grades = ["A*", "A", "B", "C", "D", "E"];
+    const grades = Object.keys(gradeValues);
     grades.forEach((grade) => {
       const input = document.createElement("input");
       input.type = "radio";
       input.name = `subject${index + 1}`;
-      input.value = grade;
+      input.value = gradeValues[grade];
       input.id = `subject${index + 1}-${grade}`;
 
       const gradeLabel = document.createElement("label");
@@ -49,4 +72,47 @@ function generateSubjectRadios() {
 // Call the function to generate radio buttons
 generateSubjectRadios();
 
-// Rest of your JavaScript code...
+// Function to calculate the total grade
+function calculateGrades() {
+  const totalGradeSpan = document.getElementById("totalGrade");
+  const percentageSpan = document.getElementById("percentage");
+
+  const subjectValues = [];
+
+  // Collect the selected grades for all subjects
+  for (let i = 1; i <= subjects.length; i++) {
+    const selectedGrade = document.querySelector(
+      `input[name="subject${i}"]:checked`
+    );
+    if (!selectedGrade) {
+      alert(`Please select a grade for ${subjects[i - 1]}.`);
+      return; // Exit if any subject is not selected
+    }
+
+    const subjectName = subjects[i - 1];
+    const grade = selectedGrade.nextSibling.textContent.trim(); // Get the grade text
+    const customMark =
+      customMarks[subjectName] && customMarks[subjectName][grade];
+    const value =
+      customMark !== undefined ? customMark : parseInt(selectedGrade.value);
+
+    subjectValues.push(value);
+  }
+
+  // Calculate the sum of grades
+  const totalGrade = subjectValues.reduce((acc, value) => acc + value, 0);
+
+  // Calculate the percentage
+  const maxTotal =
+    Object.values(gradeValues).reduce((acc, value) => acc + value, 0) *
+    subjects.length;
+  const percentage = (totalGrade / maxTotal) * 100;
+
+  // Display the results
+  totalGradeSpan.textContent = totalGrade;
+  percentageSpan.textContent = percentage.toFixed(2); // Display percentage with 2 decimal places
+}
+
+// Attach the calculateGrades function to the button click event
+const calculateButton = document.querySelector("button");
+calculateButton.addEventListener("click", calculateGrades);
